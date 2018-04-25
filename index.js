bcde670ab935e878d141a0f3afe80d9c6149b0c9
gguo <gguo@movoto.com>
2018-04-25 16:09:55 -0700

    added basics

---
+++ index.js
@@ -0,0 +1,11 @@
+const browser = require('./lib/browser');
+const formatter = require('./lib/formatter');
+const mixins = require('./lib/mixins');
+const validator = require('./lib/validator');
+
+module.exports = {
+    browser,
+    formatter,
+    mixins,
+    validator
+};
